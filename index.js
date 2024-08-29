2d9baa1ffdaf6ae1f606f9586794fb0f487ef204
jayspatel <jayspatel2153@gmail.com>
2024-08-29 16:17:56 +0530

    first

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const express = require('express');
 const app = express();
 
 // Use the PORT variable from .env
-const PORT = process.env.PORT;
+const PORT = process.env.API_PORT;
 
 app.get('/', (req, res) => {
-  res.send(`Hello DevOps ! in ${process.env.NODE_ENV}`);
+  res.send(`Hello Jayy DevOps.....! in ${process.env.MY_ENV_NAME}`);
 });
 
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`);
+  console.log(`Server is running on port ${PORT} in ${process.env.MY_ENV_NAME} mode`);
 });
