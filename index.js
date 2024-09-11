1fb32abac2082260dfe8a54f4be70391b7e45b29
jayspatel21 <jay.patel@linearloop.io>
2024-09-11 16:27:32 +0530

    none-11

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.API_PORT;
 
 app.get('/', (req, res) => {
-  res.send(`Hello Jayyyyyyyyyyyyydddyyyy DevOpsdddddddddddd..... ECS-ECR new!!!!!!! ${process.env.ENV_NAME}`);
+  res.send(`Hello Devopsss... ECS-ECR  ${process.env.ENV_NAME}`);
 });
 
 
